5850fe341881332caa97a83db718537a69d464c6
sumitsharma372 <sksharma372@kgpian.iitkgp.ac.in>
2022-11-06 19:43:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Genres from './Components/Genres';
+import Header from './Components/Header';
+import About from './Components/About';
+import Features from './Components/Features';
+import Cart from './Components/Cart';
+import Footer from './Components/Footer';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Router>
+        <Switch>
+          <Route exact path = '/'>
+            <Header/>
+            <Genres/>
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+          <Route exact path = "/features">
+            <Features/>
+          </Route>
+          <Route exact path = "/cart">
+            <Cart/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
